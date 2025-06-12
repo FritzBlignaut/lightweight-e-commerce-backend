522753c0e4af117613828b54ec5ca267510f497e
Fritz Blignaut <fritz.blignaut@gmail.com>
2025-06-12 10:26:29 +1000

    feat: implement search, price filtering, and pagination in Product service and controller

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,10 @@
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 import { ProductService } from './product.service';
 import { RolesGuard } from 'src/auth/guards/roles.guard';
 import { CreateProductDto } from './dto/create-product.dto';
 import { Roles } from 'src/auth/decorators/roles.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
-import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
 
 @ApiBearerAuth('access_token')
 @UseGuards(JwtAuthGuard, RolesGuard)
@@ -13,9 +13,25 @@ import { Body, Controller, Delete, Get, Param, Post, UseGuards } from '@nestjs/c
 export class ProductController {
     constructor(private productService: ProductService) {}
 
+    @ApiQuery({ name: 'search', required: false })
+    @ApiQuery({ name: 'minPrice', required: false })
+    @ApiQuery({ name: 'maxPrice', required: false })
+    @ApiQuery({ name: 'limit', required: false })
+    @ApiQuery({ name: 'offset', required: false })
     @Get()
-    findAll() {
-        return this.productService.findAll();
+    findAll(
+        @Query('search') search?: string,
+        @Query('minPrice') minPrice?: number,
+        @Query('maxPrice') maxPrice?: number,
+        @Query('limit') limit = 10,
+        @Query('offset') offset = 0,) {
+        return this.productService.findAll({
+            search,
+            minPrice: Number(minPrice),
+            maxPrice: Number(maxPrice),
+            limit: Number(limit),
+            offset: Number(offset),
+        });
     }
 
     @Get(':id')
